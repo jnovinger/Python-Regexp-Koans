a2900b197dfc2e4d820bdd3fe0269e6d6194e7fc
Tomas Nilsson <tnilsson.x@gmail.com>
2011-06-23 20:34:19 +0200

    Updated path
    - AboutMatch added
    - AboutSearch added
    - AboutFindall added

--- python2/runner/path_to_enlightenment.py
+++ python2/runner/path_to_enlightenment.py
@@ -6,11 +6,15 @@
 import unittest
 
 from koans.about_match import AboutMatch
+from koans.about_search import AboutSearch
+from koans.about_findall import AboutFindall
 
 def koans():
     loader = unittest.TestLoader()
     suite = unittest.TestSuite()
     loader.sortTestMethodsUsing = None
     suite.addTests(loader.loadTestsFromTestCase(AboutMatch))
+    suite.addTests(loader.loadTestsFromTestCase(AboutSearch))
+    suite.addTests(loader.loadTestsFromTestCase(AboutFindall))
 
     return suite
