23c08d46c122b0f1db03553eefb1dfc13ad64d02
Tomas Nilsson <tnilsson.x@gmail.com>
2011-06-23 19:34:43 +0200

    Updated AboutMatch

--- python2/koans/about_match.py
+++ python2/koans/about_match.py
@@ -20,16 +20,39 @@ class AboutMatch(Koan):
         first_three_letters = "abc"
         self.assertTrue(re.match(__, "abcdef"))
 
-    def test_match_must_start_at_the_beginning(self):
-        """
-        If no match is found, None is returned
-        """
-        third_letter = "c"
-        self.assertEqual(__, re.match(third_letter, "abcdef"))
-
     def test_empty_string_is_also_a_match(self):
         """
-        An empty string produces a zero-length match
+        An empty string produces a zero-length match.
         """
         empty_string = ""
         self.assertTrue(re.match(__, "abcdef"))
+
+    def test_no_match(self):
+        """
+        If no match is found, None is returned.
+        """
+        non_existing_char = "q"
+        self.assertEqual(None, re.match(___, "abcdef"))
+        
+        # You can also test for False
+        self.assertFalse(re.match(__, "abcdef"))
+
+    def test_match_must_start_at_the_beginning(self):
+        """
+        The pattern must match at the beggining of the string. If no match is
+        found, None is returned.
+        """
+        third_letter = "c"
+        self.assertEqual(None, re.match(___, "abcdef"))
+
+    def test_a_successful_match_returns_a_match_object(self):
+        """
+        A successful match returns a match object.
+        """
+        m = re.match("a", "abcdef")
+        self.assertEqual("<type '_sre.SRE_Match'>", str(type(m)))
+        
+        # As explained before, _sre.SRE_Match objects always have a boolean
+        # value of True.
+        self.assertEqual(True, ___)
+ 
